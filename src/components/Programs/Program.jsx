c1118973b13cbd7908e7970329ce34ecda39c294
Arsenii Bylym <arseniibylym@gmail.com>
2019-07-09 18:48:58 +0300

    Add smoove picture toggle on Program page

--- src/components/Programs/Program.jsx
+++ src/components/Programs/Program.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import styles from './styles.module.scss';
 import {useStoreState} from 'easy-peasy';
 import Grid from '@material-ui/core/Grid';
@@ -6,32 +6,22 @@ import { styleCompose } from './../../helpers';
 
 
 export const Program = props => {
-    const [image, setImage] = useState(null);
     const [selected, setSelected] = useState(0)
     const lang = useStoreState(state => state.lang.current);
     const {id, name, index, items} = props;
 
-    useEffect(() => {
-        setImage(items[0].image)
-    }, [])
-    
-
-    const itemClickHandler = (index, image)  => e => {
-        setSelected(index);
-        setImage(image);
-    }
-
-    const getImageStyle = () => {
-        const selected = image ? styles.image_selected : null;
-        return styleCompose(styles.image, selected);
-    }
-
     const getProgramItems = () => {
         return items.map((item, i) => {
-            const textPosition = i % 2 === 0 ? styles.ltr : styles.rtl;
-            const isSelected = i === selected ? styles.selected : null;
+            const textDirectionStyle = i % 2 === 0 ? styles.ltr : styles.rtl;
+            const selectedStyle = i === selected ? styles.selected : null;
+
             return (
-                <Grid onClick={itemClickHandler(i, item.image)} key={item.name[lang]} item className={styleCompose(styles.item, textPosition, isSelected)}>
+                <Grid 
+                    key={item.name[lang]} 
+                    item 
+                    onClick={() => setSelected(i)} 
+                    className={styleCompose(styles.item, textDirectionStyle, selectedStyle)}
+                >
                     <div className={styles.item__header}>{i + 1}. {item.name[lang]}</div>
                     <div className={styles.item__content}>{item.details[lang]}</div>
                 </Grid>
@@ -39,14 +29,32 @@ export const Program = props => {
         })
     }
 
+    
+    const getImages = () => {
+        return items.map((item, index) => (
+            <div 
+                key={item.name[lang]} 
+                className={getImageStyle(index)} 
+                style={{backgroundImage: `url(${item.image})`}} 
+            />
+        ))
+    }
+
+    const getImageStyle = (index) => {
+        const isActiveStyle = index === selected ? styles.image__active : null;
+        return styleCompose(styles.image, isActiveStyle)
+    }
+
     return (
         <div className={styles.root} id={id}>
-            <div className={styles.header}>{name[lang]}</div>
+            <div className={styles.header}>
+                {name[lang]}
+            </div>
             <Grid container className={styles.container}>
                 {index % 2 === 0 ? (
                     <>
                         <Grid item xs={12} md={5} className={styles.image__wrapper}>
-                            <div className={getImageStyle()} style={image && {backgroundImage: `url(${image})`}} />
+                            {getImages()}
                         </Grid>
                         <Grid item xs={12} md={7} className={styles.content} container>
                             {getProgramItems()}
@@ -58,7 +66,7 @@ export const Program = props => {
                             {getProgramItems()}
                         </Grid>
                         <Grid item xs={12} md={5} className={styles.image__wrapper}>
-                            <div className={getImageStyle()} style={image && {backgroundImage: `url(${image})`}} />
+                            {getImages()}
                         </Grid>
                     </>
                 )}
