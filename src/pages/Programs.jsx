6fc909c9902002635fd66a985173bbc3dcdd48f6
Arsenii Bylym <arseniibylym@gmail.com>
2019-07-09 18:55:02 +0300

    Program styles small fixes

--- src/pages/Programs.jsx
+++ src/pages/Programs.jsx
@@ -4,6 +4,8 @@ import {Spinner, DetailsPageHeader} from '../components/shared';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import {Program} from '../components/Programs'
+import {Particles} from '../components/shared'
+
 
 import { Api } from '../api/index';
 import {data} from '../data/index'
@@ -37,10 +39,13 @@ const Programs = () => {
 
     if (!programs || !content) return <Spinner />;
     return (
+        <>
+        <Particles />
         <div className="PageDetails Programs">
             <DetailsPageHeader title={content.title} description={content.description} />
             <div className={styles.programs}>{getPrograms()}</div>
         </div>
+        </>
     );
 };
 
