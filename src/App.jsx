b063a15670d05891a810640fb0ecfc0687ec27cf
Arsenii Bylym <arseniibylym@gmail.com>
2019-07-14 15:59:50 +0300

    Add lazy loading for layout and admin pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Admin, Home, Layout} from './pages';
+import Home from './pages/Home';
+import { Spinner } from './components/shared';
+
+const Layout = lazy(() => import('./pages/Layout'));
+const Admin = lazy(() => import('./pages/Admin'));
 
 function App() {
     return (
         <div>
             <Router>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/admin" component={Admin} />
-                    <Route path="/" component={Layout} />
-                </Switch>
+                <Suspense fallback={<Spinner />}>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/admin" component={Admin} />
+                        <Route path="/" component={Layout} />
+                    </Switch>
+                </Suspense>
             </Router>
         </div>
     );
