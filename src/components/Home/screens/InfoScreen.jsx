3848631c411ead9f339c6ed147119a79070866df
Arsenii Bylym <arseniibylym@gmail.com>
2019-08-02 09:39:01 +0300

    Info screen done

--- src/components/Home/screens/InfoScreen.jsx
+++ src/components/Home/screens/InfoScreen.jsx
@@ -1,36 +1,52 @@
 import React, {useState} from 'react';
 import Grid from '@material-ui/core/Grid';
+import {IoIosArrowDown} from 'react-icons/io';
+
 import './styles.scss';
 import VisibilitySensor from 'react-visibility-sensor';
 
 export const InfoScreen = props => {
     const [visible, setVisible] = useState(false)
-    const {text} = props;
+    const {header, subHeader, text, nextScreenTitle} = props;
+    console.log(props)
     return (
-        <div className={`InfoScreen`}>
+        <div className='InfoScreen'>
             <VisibilitySensor 
                 partialVisibility={true}
                 active={!visible}
                 onChange={(isVisible) => setVisible(isVisible)}
             >
                 {({isVisible}) => (
-                    <div className={isVisible ? "wrapper visible" : "wrapper"}>
-                        <Grid container spacing={4}>
-                            {text.map(item => (
-                                <Grid
-                                    key={item.title}
-                                    item
-                                    xs={12}
-                                    sm={6}
-                                    md={3}
-                                    className="element"
-                                >
-                                    <div className="title">{item.title}</div>
-                                    <div className="subtitle">{item.subtitle}</div>
-                                </Grid>
-                            ))}
+                    <Grid container className="InfoScreen__content" justify="center" direction="column" wrap="nowrap" style={{height: '100%'}}>
+                        <Grid item xs={12} container alignItems="flex-end" justify="center">
+                            <div className="InfoScreen__header">{header}</div>
                         </Grid>
-                    </div>
+                        <Grid item xs={12} container justify="center" alignItems="center"  >
+                            <div className="InfoScreen__subHeader" dangerouslySetInnerHTML={{__html: subHeader}} />
+                        </Grid>
+                        <Grid item xs={12} className={isVisible ? "wrapper visible" : "wrapper"}>
+                            <Grid container spacing={4}>
+                                {text.map(item => (
+                                    <Grid
+                                        key={item.title}
+                                        item
+                                        xs={12}
+                                        sm={4}
+                                        className="element"
+                                    >
+                                        <div className="title">{item.title}</div>
+                                        <div className="subtitle">{item.subtitle}</div>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} container justify="flex-end" direction="column" wrap="nowrap" alignItems="center" className="InfoScreen__footer" >
+                            <div className="button">{nextScreenTitle}</div>
+                            <div className="icon-container">
+                                <IoIosArrowDown className="icon" />
+                            </div>
+                        </Grid>
+                    </Grid>
                 )}
             </VisibilitySensor>
         </div>
