a2e0722dff020af3e4805990677973750bd37696
Arsenii Bylym <arseniibylym@gmail.com>
2019-08-03 16:00:40 +0300

    contact screen small style changes

--- src/components/Home/screens/ContactsScreen.jsx
+++ src/components/Home/screens/ContactsScreen.jsx
@@ -8,33 +8,46 @@ export const ContactsScreen = props => {
     const {address} = props;
     const {phones, email, facebook, development} = data.core.contacts;
     return (
-        <div className={`ContactsScreen`}>
-            <div className="address">{address}</div>
-            {phones.map(item => (
-                <div key={item} className="phone">
-                    {item}
+        <VisibilitySensor
+            partialVisibility={true}
+            offset={{top: 0, bottom: 300}}
+            // active={!visible}
+            // onChange={isVisible => setVisible(isVisible)}
+        >
+            {({isVisible}) => (
+                <div className={isVisible ? 'ContactsScreen visible' : 'ContactsScreen'}>
+                    <div className="address">{address}</div>
+                    {phones.map(item => (
+                        <div key={item} className="phone">
+                            {item}
+                        </div>
+                    ))}
+                    <div className="email">
+                        <a href={`mailto:${email}`}>
+                            <MdMailOutline />
+                            <p>{email}</p>
+                        </a>
+                    </div>
+                    <div className="socials">
+                        <a
+                            href={`https://www.${facebook}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <IoLogoFacebook />
+                        </a>
+                    </div>
+                    <div className="development">
+                        <div className="copyright">
+                            &#169; {new Date().getFullYear()} All rights reserved
+                        </div>
+                        <div className="devInfo">
+                            <span>Developed by </span>
+                            <a href={`mailto:${development.email}`}>{development.name}</a>
+                        </div>
+                    </div>
                 </div>
-            ))}
-            <div className="email">
-                <a href={`mailto:${email}`}>
-                    <MdMailOutline />
-                    <p>{email}</p>
-                </a>
-            </div>
-            <div className="socials">
-                <a href={`https://www.${facebook}`} target="_blank" rel="noopener noreferrer" >
-                    <IoLogoFacebook />
-                </a>
-            </div>
-            <div className="development">
-                <div className="copyright">
-                    &#169; {new Date().getFullYear()} All rights reserved
-                </div>
-                <div className="devInfo">
-                    <span>Developed by </span>
-                    <a href={`mailto:${development.email}`}>{development.name}</a>
-                </div>
-            </div>
-        </div>
+            )}
+        </VisibilitySensor>
     );
 };
